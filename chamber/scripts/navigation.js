06948e7a9333b659414f1369814d2f947aed50e5
Doriskhosla <doriskhosla@gmail.com>
2026-07-21 21:47:22 -0600

    Update the new changes in index.html, navigation.js, chamber-larger.css and chamber-small.css

--- chamber/scripts/navigation.js
+++ chamber/scripts/navigation.js
@@ -1,11 +1,11 @@
 
-const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('#navMenu');
+const hamButton = document.querySelector("#hamburger");
+const navigation = document.querySelector("#navMenu");
 
-hamButton.addEventListener('click', () => {
-    navigation.classList.toggle('show');
-    hamButton.classList.toggle('show');
+hamButton.addEventListener("click", () => {
+    navigation.classList.toggle("show");
 
-    const isOpen = navigation.classList.contains('show');
-    hamButton.setAttribute('aria-expanded', isOpen);
+    const isOpen = navigation.classList.contains("show");
+    hamButton.setAttribute("aria-expanded", isOpen);
 });
+
