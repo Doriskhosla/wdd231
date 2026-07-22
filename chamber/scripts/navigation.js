80fd01c2d2e2042f2a1e9e9a59524f0c088c9634
Doriskhosla <doriskhosla@gmail.com>
2026-07-21 22:54:57 -0600

    Commit all changes in chamber folder.

--- chamber/scripts/navigation.js
+++ chamber/scripts/navigation.js
@@ -4,6 +4,7 @@ const navigation = document.querySelector("#navMenu");
 
 hamButton.addEventListener("click", () => {
     navigation.classList.toggle("show");
+    hamButton.classList.toggle("show");
 
     const isOpen = navigation.classList.contains("show");
     hamButton.setAttribute("aria-expanded", isOpen);
